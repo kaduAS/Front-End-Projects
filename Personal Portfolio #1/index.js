4d50dab4ad87850c2344f636a45ebd396d0253b3
Carlos Eduardo <kadusilvaamaral@gmail.com>
2025-07-03 14:15:47 -0300

    Add Personal Portfolio #1

---
+++ Personal Portfolio #1/index.js
@@ -0,0 +1,15 @@
+//let FavoriteFood = "pizza";
+//let email = "kadusilvaamaral@gmail.com";
+
+//console.log(`Your email is ${email}`);
+//console.log(`You like ${FavoriteFood}`);
+
+//let online = false;
+//let ForSale = true;
+
+//console.log(`Cadu is ${online}`);
+//console.log(`Is this car for sale ${ForSale}`);
+
+let FullName = "Carlos Eduardo";
+let age = 17;
+let student = true;
